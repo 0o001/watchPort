9b8cc8bdde4dc64b515d4e88ada1cea5f4c6407e
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2019-10-14 18:50:15 +0300

    Create portWatch.py

---
+++ portWatch.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+def clearConsole():
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
+
+def runCommand():
+    command = os.popen('netstat -tulpn 2>/dev/null | grep "LISTEN"')
+    result = command.read().strip()
+    command.close()
+
+    print(result)
+
+if __name__ == '__main__':
+    try:
+        while True:
+            clearConsole()
+            runCommand()
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
